89a36b17770f7f9d90d0410a4d5ce8a22bf3a3a2
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2024-07-22 22:02:03 +0300

    Update Basamak.cpp

--- src/Basamak.cpp
+++ src/Basamak.cpp
@@ -1,12 +1,3 @@
-/**
-* @file Basamak.cpp
-* @description Basamak sınıfının cpp dosyası.
- Gerekli açıklamalar Basamak.hpp dosyasında yapılmışıtr.
-* @course 1. Öğretim B Grubu
-* @assignment 1. Ödev
-* @date 25/11/2023 (TAM HALİ)
-* @author Eren Yurtaslan abdulkadir.yurtaslan@ogr.sakarya.edu.tr
-*/
 
 
 #include "Basamak.hpp"
