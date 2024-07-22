0b9578571e6fa1504e12b106bdcdcbfe26cb68d1
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2024-07-22 22:02:33 +0300

    Update Dugum.hpp

--- include/Dugum.hpp
+++ include/Dugum.hpp
@@ -1,13 +1,3 @@
-/**
-* @file Dugum.hpp
-* @description İçinde Sayi* türünden sayi isminde değer tutan, Dugum* türünden "sonraki" isminde pointer saklayan,
- SayilarListesi listesinin düğüm sınıfı.
-
-* @course 1. Öğretim B Grubu
-* @assignment 1. Ödev
-* @date 25/11/2023 (TAM HALİ)
-* @author Eren Yurtaslan abdulkadir.yurtaslan@ogr.sakarya.edu.tr
-*/
 
 
 #ifndef DUGUM_HPP
