89589f56300d74c536b28d3310300e6ad51ff1f3
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2024-07-22 22:02:52 +0300

    Update SayilarListesi.hpp

--- include/SayilarListesi.hpp
+++ include/SayilarListesi.hpp
@@ -1,20 +1,3 @@
-/**
-* @file SayilarListesi.hpp
-
-
-* @description SayilarListesi isimli bağlı listenin header dosyası.
- Dugum.hpp'yi include ederek onun özelliklerini alır ve düğümlerini "Dugum" türünde oluşturur.
- Dugum* türünde bir pointer oluşturarak bunu ilk elemanı gösterecek biçimde ayarlar.
- Hemen altında, SayilarListesi.cpp'de implement edilecek olan fonksiyonlar yazmakta.
-
-
-* @course 1. Öğretim B Grubu
-* @assignment 1. Ödev
-* @date 25/11/2023 (TAM HALİ)
-* @author Eren Yurtaslan abdulkadir.yurtaslan@ogr.sakarya.edu.tr
-*/
-
-
 
 #ifndef SAYILAR_LISTESI_HPP
 #define SAYILAR_LISTESI_HPP
