2990a39c145726e553bb5925faa189431456c563
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2024-07-22 22:00:43 +0300

    Update Dugum.cpp

--- src/Dugum.cpp
+++ src/Dugum.cpp
@@ -1,12 +1,4 @@
-/**
-* @file Dugum.cpp
-* @description Dugum sınıfının cpp dosyası.
- Gerekli açıklamalar Dugum.hpp dosyasında yapılmışıtr.
-* @course 1. Öğretim B Grubu
-* @assignment 1. Ödev
-* @date 25/11/2023 (TAM HALİ)
-* @author Eren Yurtaslan abdulkadir.yurtaslan@ogr.sakarya.edu.tr
-*/
+
 
 #include "Dugum.hpp"
 
