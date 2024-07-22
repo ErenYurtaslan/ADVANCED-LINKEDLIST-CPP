a66467b3ca5b419d2fba27f6f377f6605b130c09
Eren Yurtaslan <92111356+ErenYurtaslan@users.noreply.github.com>
2024-07-22 22:02:26 +0300

    Update Basamak.hpp

--- include/Basamak.hpp
+++ include/Basamak.hpp
@@ -1,16 +1,3 @@
-/**
-* @file Basamak.hpp
-
-* @description Bir node oluşturma dosyası.
- Sayi listesinin node cinsi işte bu sınıftır.
-  Tuttuğu değer sayısal basamak olduğu için haliyle integerdır, ve "sonraki" isimli pointer ile
-  bir sonraki adresteki "Basamak" düğümüne ulaşılır.
-
-* @course 1. Öğretim B Grubu
-* @assignment 1. Ödev
-* @date 25/11/2023 (TAM HALİ)
-* @author Eren Yurtaslan abdulkadir.yurtaslan@ogr.sakarya.edu.tr 
-*/
 
 #ifndef BASAMAK_HPP
 #define BASAMAK_HPP
